f4a8b9abbf33287a45929c56a52a6c1c83778070
Enis Afgan <afgane@gmail.com>
2015-09-21 09:48:58 -0400

    Update cloudman-image role

---
+++ roles/galaxyprojectdotorg.cloudman-image/files/BatchJobs_global_config.R
@@ -0,0 +1,7 @@
+cluster.functions = makeClusterFunctionsSLURM("/usr/local/lib/R/site-library/BatchJobs/etc/simple.tmpl")
+mail.start = "none"
+mail.done = "none"
+mail.error = "none"
+db.driver = "SQLite"
+db.options = list()
+debug = FALSE
