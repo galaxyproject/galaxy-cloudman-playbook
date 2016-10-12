7127bb467f5cad3b43312e55fa8f7edb7308a3e0
Enis Afgan <afgane@gmail.com>
2016-10-12 12:13:06 -0400

    Remove cloudman-image source from the repo
    (We'll include it as an independently installable dependency.)

--- roles/galaxyprojectdotorg.cloudman-image/files/BatchJobs_global_config.R
+++
@@ -1,7 +0,0 @@
-cluster.functions = makeClusterFunctionsSLURM("/usr/local/lib/R/site-library/BatchJobs/etc/simple.tmpl")
-mail.start = "none"
-mail.done = "none"
-mail.error = "none"
-db.driver = "SQLite"
-db.options = list()
-debug = FALSE
